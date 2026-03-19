b60ed66bb967c5912017c8e58de358529756a5af
Khalil Alobeid <philotheories@gmail.com>
2026-03-19 08:53:36 -0400

    Finished login and register pages

--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrls: ['./login.css'],
 })
-export class Login {}
+export class Login {
+  email = '';
+  password = '';
+
+  signIn(form: any) {
+    if (form.valid) {
+      console.log('User logged in:', form.value);
+    }
+  }
+}
--- src/app/pages/auth/register/register.ts
+++ src/app/pages/auth/register/register.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './register.html',
-  styleUrl: './register.css',
+  styleUrls: ['./register.css'],
 })
-export class Register {}
+export class Register {
+  name = '';
+  email = '';
+  password = '';
+
+  register(form: any) {
+    if (form.valid) {
+      console.log('User Registered:', form.value);
+    }
+  }
+}
