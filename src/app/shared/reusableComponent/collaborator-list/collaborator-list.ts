b69be76b6858bd2406ce905bf2a6fe86421681a7
tingm3 <tingyue.dong@mthree.com>
2026-03-20 13:44:54 -0400

    refactor into front/backend

--- src/app/shared/reusableComponent/collaborator-list/collaborator-list.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-collaborator-list',
-  imports: [],
-  templateUrl: './collaborator-list.html',
-  styleUrl: './collaborator-list.css',
-})
-export class CollaboratorList {}
--- src/app/shared/reusableComponent/user-cursor/user-cursor.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-user-cursor',
-  imports: [],
-  templateUrl: './user-cursor.html',
-  styleUrl: './user-cursor.css',
-})
-export class UserCursor {}
