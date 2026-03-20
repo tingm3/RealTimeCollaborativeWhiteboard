07d221ac16364ec2cbcaeffddb2974648287a857
tingm3 <tingyue.dong@mthree.com>
2026-03-20 13:40:33 -0400

    Revert "refactor into frontend backend folder"
    This reverts commit 0ea0dfefed730293f918d8b83ebce1339b0272e7.

--- frontend/src/app/content/content.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-content',
-  imports: [],
-  templateUrl: './content.html',
-  styleUrl: './content.css',
-})
-export class Content {}
--- frontend/src/app/shared/reusableComponent/color-picker/color-picker.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-color-picker',
-  imports: [],
-  templateUrl: './color-picker.html',
-  styleUrl: './color-picker.css',
-})
-export class ColorPicker {}
